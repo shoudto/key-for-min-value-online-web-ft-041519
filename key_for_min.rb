1e42012373fc944dbfdbf1633ce3e0d2ea5273c8
Oscar Shoudt <shoudto@gmail.com>
2019-05-02 18:52:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,6 @@ require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(hash)
-
+  min_key = 
 end 
 
